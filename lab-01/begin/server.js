358920017d0331a7ec0a519b9ca8269739f56195
Bobby Johnson <bobby.johnson@auth0.com>
2019-02-06 12:04:19 -0800

    lab complete

--- lab-01/begin/server.js
+++ lab-01/begin/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const session = require('cookie-session');
+const { auth } = require('express-openid-connect');
 
 const appUrl = process.env.BASE_URL || `http://localhost:${process.env.PORT}`;
 
@@ -13,6 +15,14 @@ app.set('view engine', 'ejs');
 app.use(morgan('combined'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({
+  name: 'identity102-l01-e01',
+  secret: process.env.COOKIE_SECRET
+ }));
+ app.use(auth({
+  auth0Logout: true
+ }));
 
 app.get('/', (req, res) => {
   res.render('home',  { user: req.openid && req.openid.user });
